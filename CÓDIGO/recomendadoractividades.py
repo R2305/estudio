379833d86b7d1ff68b7c29f7e32ab92df9be3348
R2305 <ruelasrogelio70@gmail.com>
2026-07-31 03:00:50 -0700

    recomendador actualizado

--- CÓDIGO/recomendadoractividades.py
+++ CÓDIGO/recomendadoractividades.py
@@ -8,7 +8,17 @@
 • Estado_animo: Puede ser “activo” o “relajado”."""
 
 clima =['soleado','lluvioso','nublado']
-hora= ['mañana']
-eleccion_clima= int(input())
-resultado_clima = clima[0]
-print(resultado_clima)
+hora= ['mañana', 'tarde', 'noche']
+estado_animo = ['activo', 'relajado']
+eleccion_clima= int(input("escribe:"))
+eleccion_hora= int(input())
+eleccion_animo= int(input())
+a= clima[eleccion_clima]
+b= hora[eleccion_hora]
+c= estado_animo[eleccion_animo]
+
+estado_animo= eleccion_animo
+
+
+if (eleccion_clima== 0 or eleccion_clima == 2) and eleccion_animo == 0:
+    print(' haz ejercicio')
